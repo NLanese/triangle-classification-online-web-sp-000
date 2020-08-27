b089b00e0ed8f135022d402c3376a55c76719b57
Nick Lanese <nlanese21@gmail.com>
2020-08-27 19:50:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,26 @@
 class Triangle
-  attr_accessor :kind
+  attr_accessor
 
   def initialize(s1, s2, s3)
     @s1 = s1
     @s2 = s2
     @s3 = s3
+  end
+
+  def kind
+    if (s1 == s2 && s2 == s3)
+      kind = :equilateral
+    elsif (s1 + s2 < s3 || s2 + s3 < s1 || s1 + s3 < s2)
+      #error
+    elsif (s1 == s2 || s2 == s3 || s1 == s3)
+      kind = :isosceles
+    else
+      kind = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "not a triangle"
   end
 
 
