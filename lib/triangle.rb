99133f41724ab0ada08e46c2676a0dbd1ef37cc5
Nick Lanese <nlanese21@gmail.com>
2020-08-27 20:01:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,6 +5,11 @@ class Triangle
     @s1 = s1
     @s2 = s2
     @s3 = s3
+    if (@s1 + @s2 < @s3 || @s2 + @s3 < @s1 || @s1 + @s3 < @s2)
+      raise TriangleError
+    elsif (@s1 == 0 || @s2 == 0 || @s3 == 0)
+      raise TriangleError
+    end
   end
 
   def kind
