84ff4d91cb3397077f3c140836076e4ada3fa673
Nick Lanese <nlanese21@gmail.com>
2020-08-27 20:00:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,13 +8,13 @@ class Triangle
   end
 
   def kind
-    if (s1 == s2 && s2 == s3)
+    if (@s1 == @s2 && @s2 == @s3)
       kind = :equilateral
-    elsif (s1 + s2 < s3 || s2 + s3 < s1 || s1 + s3 < s2)
+    elsif (@s1 + @s2 < @s3 || @s2 + @s3 < @s1 || @s1 + @s3 < @s2)
       raise TriangleError
-    elsif (s1 == 0 || s2 == 0 || s3 == 0)
+    elsif (@s1 == 0 || @s2 == 0 || @s3 == 0)
       raise TriangleError
-    elsif (s1 == s2 || s2 == s3 || s1 == s3)
+    elsif (@s1 == @s2 || @s2 == @s3 || @s1 == @s3)
       kind = :isosceles
     else
       kind = :scalene
